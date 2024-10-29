b288aa04fd6b6d7afec873442bf8bc11a0d7ab0b
Isaac Ortiz <isaacortiz@Isaacs-MacBook-Air.local>
2024-10-28 22:10:53 -0500

    Second commint

--- obj/Debug/net8.0/BlazorAvanzado.AssemblyInfo.cs
+++ obj/Debug/net8.0/BlazorAvanzado.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorAvanzado")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7be768b02813f45d40f4dd812445d80502cf36c5")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorAvanzado")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorAvanzado")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
