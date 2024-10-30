c69e8bbb7e1915c439ce3d5c00d539abe04db000
hardisaakpp <hardisaakpp@hotmail.es>
2024-10-29 21:07:42 -0500

    first commit

--- obj/Debug/net8.0/BlazorAvanzado.AssemblyInfo.cs
+++ obj/Debug/net8.0/BlazorAvanzado.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorAvanzado")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+118e4d63bfc799b1eb0ca9d1bf34104f7d27f861")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6303414672e7f9beb6575ea1a437af733cd65cc1")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorAvanzado")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorAvanzado")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
